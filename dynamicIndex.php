b8b5aed8955fdd03651ab89a583c1c4070e0ce19
Blake Robertson <brob@tamu.edu>
2014-04-22 21:19:07 -0500

    Added Tables and Cleaned up Code
    Added tables as one of the options to display the data, users can now
    select to see their data in graphic form or tabular form.

--- dynamicIndex.php
+++ dynamicIndex.php
@@ -20,7 +20,8 @@
 	<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false'></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
-	<script src="boxIt.js" type="text/javascript"></script>
+	<script src="boxIt.js"></script>
+	<script src="moment.js"></script>
 	<link rel="stylesheet" href="boxIt.css">
 	<style>
 		html {
@@ -38,93 +39,118 @@
 			font-family:Arial;
 			background-color:#E0E0E0;
 		}
-		table {
-			width:100%;
-			margin-left:auto;
-			margin-right:auto;
-			margin-top:14px;
-		}
-		table, td, th {
-			border:1px solid black;
-		}
-		.nodeTables {
-			text-align:left;
-			padding-top:25px;
-		}
-		.nodeTitle {
-			font-size:24px;
-			font-weight:bold;
-		}
-		canvas {
-			float:left;
-		}
-		iframe {
-			margin-top:10px;
-			margin-left:100px;
-		}
-		#nodeSelector {
-			float:left;
-			width:100%;
-			height:21px;
-			text-align:center;
-			background-color:white;
-		}
-		.nodeSelection {
-			float:left;
-			margin-left:1px;
-			height:20px;
-			background-color:gray;
-			color:white;
-			font-weight:bold;
-			cursor:pointer;
-		}
-		#sensorSelector {
-			float:left;
-			width:199px;
-			height:410px;
-			background-color:gray;
-			border-left:1px solid white;
-		}
-		.sensorSelection {
-			padding-top:15px;
-			padding-bottom:15px;
-			border-bottom:1px solid white;
-			color:white;
-			font-weight:bold;
-			font-size:18px;
-			cursor:pointer;
-		}
 		#chart {
 			border:1px solid white;
+			-moz-box-shadow:    4px 4px 5px 2px gray;
+			-webkit-box-shadow: 4px 4px 5px 2px gray;
+			box-shadow:         4px 4px 5px 2px gray;
 		}
 		#selectors {
 			text-align:left;
 			background-color:gray;
 			float:left;
-			width:800px;
+			width:798px;
 			height:40px;
+			border-left:1px solid white;
+			border-right:1px solid white;
+			--moz-box-shadow:   4px 4px 5px 2px gray;
+			-webkit-box-shadow: 4px 4px 5px 2px gray;
+			box-shadow:         4px 4px 5px 2px gray;
 		}
 		.selectboxit-container {
 			margin-top:4px;
-			margin-left:151px;
+			margin-left:69px;
 		}
 		.selectboxit-container .selectboxit-options {
 			width:141px;
 		}
+		table{
+			border-collapse: collapse;
+			border-spacing: 0;
+			width:100%;
+			height:100%;
+			margin:0px;padding:0px;
+		}tr:last-child td:last-child {
+			-moz-border-radius-bottomright:0px;
+			-webkit-border-bottom-right-radius:0px;
+			border-bottom-right-radius:0px;
+		}
+		table tr:first-child td:first-child {
+			-moz-border-radius-topleft:0px;
+			-webkit-border-top-left-radius:0px;
+			border-top-left-radius:0px;
+		}
+		table tr:first-child td:last-child {
+			-moz-border-radius-topright:0px;
+			-webkit-border-top-right-radius:0px;
+			border-top-right-radius:0px;
+		}tr:last-child td:first-child{
+			-moz-border-radius-bottomleft:0px;
+			-webkit-border-bottom-left-radius:0px;
+			border-bottom-left-radius:0px;
+		}tr:hover td{
+			background-color:lightslategray;
+		}
+		tr:nth-child(odd) { background-color:#aad4ff; }
+		tr:nth-child(even) { background-color:#ffffff; }
+		td{
+			vertical-align:middle;	
+			border:1px solid #ffffff;
+			border-width:0px 1px 1px 0px;
+			text-align:left;
+			padding:7px;
+			font-size:14px;
+			font-family:Arial;
+			font-weight:normal;
+			color:#000000;
+		}tr:last-child td{
+			border-width:0px 1px 0px 0px;
+		}tr td:last-child{
+			border-width:0px 0px 1px 0px;
+		}tr:last-child td:last-child{
+			border-width:0px 0px 0px 0px;
+		}
+		tr:first-child td{
+			background:-o-linear-gradient(bottom, #005fbf 5%, #003f7f 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #005fbf), color-stop(1, #003f7f) );
+			background:-moz-linear-gradient( center top, #005fbf 5%, #003f7f 100% );
+			filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#005fbf", endColorstr="#003f7f");	background: -o-linear-gradient(top,#005fbf,003f7f);
+			background-color:#005fbf;
+			border:0px solid #ffffff;
+			text-align:center;
+			border-width:0px 0px 1px 1px;
+			font-size:14px;
+			font-family:Arial;
+			font-weight:bold;
+			color:#ffffff;
+		}
+		tr:first-child:hover td{
+			background:-o-linear-gradient(bottom, #005fbf 5%, #003f7f 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #005fbf), color-stop(1, #003f7f) );
+			background:-moz-linear-gradient( center top, #005fbf 5%, #003f7f 100% );
+			filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#005fbf", endColorstr="#003f7f");	background: -o-linear-gradient(top,#005fbf,003f7f);
+			background-color:#005fbf;
+		}
+		tr:first-child td:first-child{
+			border-width:0px 0px 1px 0px;
+		}
+		tr:first-child td:last-child{
+			border-width:0px 0px 1px 1px;
+		}
 	</style>
 	<script>
 	google.load('visualization', '1.1', {'packages':['annotationchart']});
-	google.setOnLoadCallback(drawChart);
+	google.setOnLoadCallback(drawGraphic);
 	var nodeActive = <?php echo $nodes[0][0]; ?>;
 	var tempActive = true;
 	var locationActive = false;
 	var radActive = false;
 	var co2Active = false;
+	var graphicActive = true;
+	var tableActive = false;
 	var chart;
-	function drawChart() {
+	function drawGraphic() {
 		clearChart();
-		$('.nodeSelection').css('background-color', 'gray');
-		$('#node'+nodeActive).css('background-color', 'rgba(151, 187, 205, 1.0)');
+		$('#chart').css('overflow-y', 'hidden');
+		$('#chart').css('overflow-x', 'hidden');
 		if (locationActive) {
 			drawMap();
 			return;
@@ -166,16 +192,12 @@
 			}
 		}
 		var getURL = "";
-		$('.sensorSelection').css('background-color', 'gray');
 		if (tempActive) {
 			getURL = "getTemps.php";
-			$('#temperature').css('background-color', 'rgba(151, 187, 205, 1.0)');
 		} else if (radActive) {
 			getURL = "getRad.php";
-			$('#radiation').css('background-color', 'rgba(151, 187, 205, 1.0)');
 		} else if (co2Active) {
 			getURL = "getCo2.php";
-			$('#carbonDioxide').css('background-color', 'rgba(151, 187, 205, 1.0)');
 		}
 		xmlhttp.open("GET",getURL+"?nodeid="+nodeActive,true);
 		xmlhttp.send();
@@ -183,8 +205,6 @@
 	
 	var map;
 	function drawMap() {
-		$('.sensorSelection').css('background-color', 'gray');
-		$('#location').css('background-color', 'rgba(151, 187, 205, 1.0)');
 		var xmlhttp;
 		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 			xmlhttp=new XMLHttpRequest();
@@ -211,6 +231,7 @@
 					var contentString = 'Latitude: ' + latitudes[i] + '<br>' +
 						'Longitude: ' + longitudes[i] + '<br>' +
 						'Date and Time: ' + datetimes[i];
+						
 					var infowindow = new google.maps.InfoWindow({
 						content: contentString,
 						maxWidth: 200
@@ -232,13 +253,70 @@
 		xmlhttp.send();
 	}
 	
+	function drawTable() {
+		clearChart();
+		$('#chart').css('overflow-y', 'auto');
+		var xmlhttp;
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		}
+		else {// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange=function() {
+			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+				var tableHtml = '';
+				var responseArray = xmlhttp.responseText.split(';');
+				var dataType = responseArray[0];
+				tableHtml += '<table>';
+				var datetimes = responseArray[1].split(',');
+				var maxIndex = datetimes.length-1;
+				if (locationActive) {
+					tableHtml += '<tr><th>Latitude</th><th>Longitude</th><th>Date</th></tr>';
+					var latitudes = responseArray[2].split(',');
+					var longitudes = responseArray[3].split(',');
+					for (var i=maxIndex; i>=0; i--) {
+						var dateVar = new Date(datetimes[i]);
+						tableHtml += '<tr><td>'+latitudes[i]+'</td><td>'+longitudes[i]+'</td><td>'+moment(dateVar).format('h:mm A MMMM Do, YYYY')+'</td></tr>';
+					}
+				} else {
+					tableHtml += '<tr><th>'+dataType+'</th><th>Date</th></tr>';
+					var data = responseArray[2].split(',');
+					for (var i=maxIndex; i>=0; i--) {
+						var dateVar = new Date(datetimes[i]);
+						tableHtml += '<tr><td>'+data[i]+'</td><td>'+moment(dateVar).format('h:mm A MMMM Do, YYYY')+'</td></tr>';
+					}
+				}
+				tableHtml += '</table>';
+				$('#chart').html(tableHtml);
+				
+			}
+		}
+		var getURL = "";
+		if (tempActive) {
+			getURL = "getTemps.php";
+		} else if (locationActive) {
+			getURL = "getLocation.php";
+		}else if (radActive) {
+			getURL = "getRad.php";
+		} else if (co2Active) {
+			getURL = "getCo2.php";
+		}
+		xmlhttp.open("GET",getURL+"?nodeid="+nodeActive,true);
+		xmlhttp.send();
+	}
+	
 	function tempClick() {
 		if (tempActive) return;
 		tempActive = true;
 		locationActive = false;
 		radActive = false;
 		co2Active = false;
-		drawChart();
+		if (graphicActive) {
+			drawGraphic();
+		} else if (tableActive) {
+			drawTable();
+		}
 	}
 	
 	function locationClick() {
@@ -247,7 +325,11 @@
 		locationActive = true;
 		radActive = false;
 		co2Active = false;
-		drawChart();
+		if (graphicActive) {
+			drawGraphic();
+		} else if (tableActive) {
+			drawTable();
+		}
 	}
 	
 	function radClick() {
@@ -256,7 +338,11 @@
 		locationActive = false;
 		radActive = true;
 		co2Active = false;
-		drawChart();
+		if (graphicActive) {
+			drawGraphic();
+		} else if (tableActive) {
+			drawTable();
+		}
 	}
 	
 	function co2Click() {
@@ -265,7 +351,11 @@
 		locationActive = false;
 		radActive = false;
 		co2Active = true;
-		drawChart();
+		if (graphicActive) {
+			drawGraphic();
+		} else if (tableActive) {
+			drawTable();
+		}
 	}
 	
 	function clearChart() {
@@ -277,7 +367,11 @@
 		
 		$(document).on('change', '.nodeSelect', function() {
 			nodeActive = $(this[this.selectedIndex]).val();
-			drawChart();
+			if (graphicActive) {
+				drawGraphic();
+			} else if (tableActive) {
+				drawTable();
+			}
 		});
 		
 		$(document).on('change', '.sensorSelect', function() {
@@ -290,6 +384,19 @@
 				radClick();
 			} else if (selection == 'carbonDioxide') {
 				co2Click();
+			}
+		});
+		
+		$(document).on('change', '.displaySelect', function() {
+			var selection = $(this[this.selectedIndex]).val();
+			if (selection == 'graphic') {
+				graphicActive = true;
+				tableActive = false;
+				drawGraphic();
+			} else if (selection == 'table') {
+				graphicActive = false;
+				tableActive = true;
+				drawTable();
 			}
 		});
 	});
@@ -310,6 +417,10 @@
 			<option value="location">Location</option>
 			<option value="radiation">Radiation</option>
 			<option value="carbonDioxide">Carbon Dioxide</option>
+		</select>
+		<select class="displaySelect">
+			<option value="graphic">Graphic</option>
+			<option value="table">Table</option>
 		</select>
 	</div>
 	<div id="#responseDiv"><div id="chart" style="width:798px;height:494px;float:left;"></div></div>
